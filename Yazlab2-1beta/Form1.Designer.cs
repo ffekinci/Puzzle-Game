380e1d3f01b75ee252f766c929622bd83d77e8f6
Furkan Faruk Ekinci <ffekinci@gmail.com>
2019-03-06 21:16:27 +0300

    Add project files.

---
+++ Yazlab2-1beta/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Yazlab2_1beta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_sec = new System.Windows.Forms.Button();
+            this.btn_P1 = new System.Windows.Forms.Button();
+            this.btn_P2 = new System.Windows.Forms.Button();
+            this.btn_P3 = new System.Windows.Forms.Button();
+            this.btn_P4 = new System.Windows.Forms.Button();
+            this.btn_P5 = new System.Windows.Forms.Button();
+            this.btn_P6 = new System.Windows.Forms.Button();
+            this.btn_P7 = new System.Windows.Forms.Button();
+            this.btn_P8 = new System.Windows.Forms.Button();
+            this.btn_P9 = new System.Windows.Forms.Button();
+            this.btn_P10 = new System.Windows.Forms.Button();
+            this.btn_P11 = new System.Windows.Forms.Button();
+            this.btn_P12 = new System.Windows.Forms.Button();
+            this.btn_P13 = new System.Windows.Forms.Button();
+            this.btn_P14 = new System.Windows.Forms.Button();
+            this.btn_P15 = new System.Windows.Forms.Button();
+            this.btn_P0 = new System.Windows.Forms.Button();
+            this.btn_Mix = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_sec
+            // 
+            this.Btn_sec.Location = new System.Drawing.Point(38, 32);
+            this.Btn_sec.Margin = new System.Windows.Forms.Padding(2);
+            this.Btn_sec.Name = "Btn_sec";
+            this.Btn_sec.Size = new System.Drawing.Size(56, 19);
+            this.Btn_sec.TabIndex = 0;
+            this.Btn_sec.Text = "Seç";
+            this.Btn_sec.UseVisualStyleBackColor = true;
+            this.Btn_sec.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_P1
+            // 
+            this.btn_P1.Location = new System.Drawing.Point(195, 56);
+            this.btn_P1.Name = "btn_P1";
+            this.btn_P1.Size = new System.Drawing.Size(150, 150);
+            this.btn_P1.TabIndex = 1;
+            this.btn_P1.UseVisualStyleBackColor = true;
+            this.btn_P1.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P2
+            // 
+            this.btn_P2.Location = new System.Drawing.Point(350, 56);
+            this.btn_P2.Name = "btn_P2";
+            this.btn_P2.Size = new System.Drawing.Size(150, 150);
+            this.btn_P2.TabIndex = 1;
+            this.btn_P2.UseVisualStyleBackColor = true;
+            this.btn_P2.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P3
+            // 
+            this.btn_P3.Location = new System.Drawing.Point(506, 56);
+            this.btn_P3.Name = "btn_P3";
+            this.btn_P3.Size = new System.Drawing.Size(150, 150);
+            this.btn_P3.TabIndex = 1;
+            this.btn_P3.UseVisualStyleBackColor = true;
+            this.btn_P3.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P4
+            // 
+            this.btn_P4.Location = new System.Drawing.Point(38, 212);
+            this.btn_P4.Name = "btn_P4";
+            this.btn_P4.Size = new System.Drawing.Size(150, 150);
+            this.btn_P4.TabIndex = 1;
+            this.btn_P4.UseVisualStyleBackColor = true;
+            this.btn_P4.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P5
+            // 
+            this.btn_P5.Location = new System.Drawing.Point(194, 212);
+            this.btn_P5.Name = "btn_P5";
+            this.btn_P5.Size = new System.Drawing.Size(150, 150);
+            this.btn_P5.TabIndex = 1;
+            this.btn_P5.UseVisualStyleBackColor = true;
+            this.btn_P5.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P6
+            // 
+            this.btn_P6.Location = new System.Drawing.Point(350, 212);
+            this.btn_P6.Name = "btn_P6";
+            this.btn_P6.Size = new System.Drawing.Size(150, 150);
+            this.btn_P6.TabIndex = 1;
+            this.btn_P6.UseVisualStyleBackColor = true;
+            this.btn_P6.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P7
+            // 
+            this.btn_P7.Location = new System.Drawing.Point(506, 212);
+            this.btn_P7.Name = "btn_P7";
+            this.btn_P7.Size = new System.Drawing.Size(150, 150);
+            this.btn_P7.TabIndex = 1;
+            this.btn_P7.UseVisualStyleBackColor = true;
+            this.btn_P7.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P8
+            // 
+            this.btn_P8.Location = new System.Drawing.Point(38, 368);
+            this.btn_P8.Name = "btn_P8";
+            this.btn_P8.Size = new System.Drawing.Size(150, 150);
+            this.btn_P8.TabIndex = 1;
+            this.btn_P8.UseVisualStyleBackColor = true;
+            this.btn_P8.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P9
+            // 
+            this.btn_P9.Location = new System.Drawing.Point(194, 368);
+            this.btn_P9.Name = "btn_P9";
+            this.btn_P9.Size = new System.Drawing.Size(150, 150);
+            this.btn_P9.TabIndex = 1;
+            this.btn_P9.UseVisualStyleBackColor = true;
+            this.btn_P9.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P10
+            // 
+            this.btn_P10.Location = new System.Drawing.Point(350, 368);
+            this.btn_P10.Name = "btn_P10";
+            this.btn_P10.Size = new System.Drawing.Size(150, 150);
+            this.btn_P10.TabIndex = 1;
+            this.btn_P10.UseVisualStyleBackColor = true;
+            this.btn_P10.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P11
+            // 
+            this.btn_P11.Location = new System.Drawing.Point(506, 368);
+            this.btn_P11.Name = "btn_P11";
+            this.btn_P11.Size = new System.Drawing.Size(150, 150);
+            this.btn_P11.TabIndex = 1;
+            this.btn_P11.UseVisualStyleBackColor = true;
+            this.btn_P11.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P12
+            // 
+            this.btn_P12.Location = new System.Drawing.Point(38, 524);
+            this.btn_P12.Name = "btn_P12";
+            this.btn_P12.Size = new System.Drawing.Size(150, 150);
+            this.btn_P12.TabIndex = 1;
+            this.btn_P12.UseVisualStyleBackColor = true;
+            this.btn_P12.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P13
+            // 
+            this.btn_P13.Location = new System.Drawing.Point(194, 524);
+            this.btn_P13.Name = "btn_P13";
+            this.btn_P13.Size = new System.Drawing.Size(150, 150);
+            this.btn_P13.TabIndex = 1;
+            this.btn_P13.UseVisualStyleBackColor = true;
+            this.btn_P13.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P14
+            // 
+            this.btn_P14.Location = new System.Drawing.Point(350, 524);
+            this.btn_P14.Name = "btn_P14";
+            this.btn_P14.Size = new System.Drawing.Size(150, 150);
+            this.btn_P14.TabIndex = 1;
+            this.btn_P14.UseVisualStyleBackColor = true;
+            this.btn_P14.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P15
+            // 
+            this.btn_P15.Location = new System.Drawing.Point(506, 524);
+            this.btn_P15.Name = "btn_P15";
+            this.btn_P15.Size = new System.Drawing.Size(150, 150);
+            this.btn_P15.TabIndex = 1;
+            this.btn_P15.UseVisualStyleBackColor = true;
+            this.btn_P15.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_P0
+            // 
+            this.btn_P0.Location = new System.Drawing.Point(38, 56);
+            this.btn_P0.Name = "btn_P0";
+            this.btn_P0.Size = new System.Drawing.Size(150, 150);
+            this.btn_P0.TabIndex = 1;
+            this.btn_P0.UseVisualStyleBackColor = true;
+            this.btn_P0.Click += new System.EventHandler(this.btnClick);
+            // 
+            // btn_Mix
+            // 
+            this.btn_Mix.Location = new System.Drawing.Point(600, 32);
+            this.btn_Mix.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Mix.Name = "btn_Mix";
+            this.btn_Mix.Size = new System.Drawing.Size(56, 19);
+            this.btn_Mix.TabIndex = 0;
+            this.btn_Mix.Text = "Karıştır";
+            this.btn_Mix.UseVisualStyleBackColor = true;
+            this.btn_Mix.Click += new System.EventHandler(this.btn_Mix_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 741);
+            this.Controls.Add(this.btn_P15);
+            this.Controls.Add(this.btn_P11);
+            this.Controls.Add(this.btn_P14);
+            this.Controls.Add(this.btn_P10);
+            this.Controls.Add(this.btn_P7);
+            this.Controls.Add(this.btn_P6);
+            this.Controls.Add(this.btn_P13);
+            this.Controls.Add(this.btn_P9);
+            this.Controls.Add(this.btn_P3);
+            this.Controls.Add(this.btn_P5);
+            this.Controls.Add(this.btn_P12);
+            this.Controls.Add(this.btn_P8);
+            this.Controls.Add(this.btn_P2);
+            this.Controls.Add(this.btn_P4);
+            this.Controls.Add(this.btn_P0);
+            this.Controls.Add(this.btn_P1);
+            this.Controls.Add(this.btn_Mix);
+            this.Controls.Add(this.Btn_sec);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_sec;
+        private System.Windows.Forms.Button btn_P1;
+        private System.Windows.Forms.Button btn_P2;
+        private System.Windows.Forms.Button btn_P3;
+        private System.Windows.Forms.Button btn_P4;
+        private System.Windows.Forms.Button btn_P5;
+        private System.Windows.Forms.Button btn_P6;
+        private System.Windows.Forms.Button btn_P7;
+        private System.Windows.Forms.Button btn_P8;
+        private System.Windows.Forms.Button btn_P9;
+        private System.Windows.Forms.Button btn_P10;
+        private System.Windows.Forms.Button btn_P11;
+        private System.Windows.Forms.Button btn_P12;
+        private System.Windows.Forms.Button btn_P13;
+        private System.Windows.Forms.Button btn_P14;
+        private System.Windows.Forms.Button btn_P15;
+        private System.Windows.Forms.Button btn_P0;
+        private System.Windows.Forms.Button btn_Mix;
+    }
+}
+
---
+++ Yazlab2-1beta/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yazlab2_1beta
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        Dictionary<int, byte[]> shuffleHash;
+        int buttonId = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public int[] Shuffle()
+        {
+            Random rastgele = new Random();
+            var rand = new int[16];
+            for (int i = 0; i < 16; i++)
+            {
+                rand[i] = i;
+            }
+
+            for (int i = 15; i >= 0; i--)
+            {
+                int sayi = rastgele.Next(9);
+                int tmp = rand[i];
+                rand[i] = rand[sayi];
+                rand[sayi] = tmp;
+            }
+
+            return rand;
+        }
+
+        public void Mix(Bitmap[,] bmps)
+        {
+            shuffleHash = new Dictionary<int, byte[]>();
+            var random = Shuffle();
+            int a = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    Button foobar = (Button)Controls.Find("btn_P" + random[a], false)[0];
+
+
+                    foobar.BackgroundImage = bmps[i, k];
+
+                    ImageConverter converter = new ImageConverter();
+                    byte[] rawImageData = converter.ConvertTo(foobar.BackgroundImage, typeof(byte[])) as byte[];
+                    MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                    byte[] hash = md5.ComputeHash(rawImageData);
+
+                    shuffleHash.Add(random[a], hash);
+                    
+                    Dictionary<int, byte[]>.ValueCollection DegerListesi = shuffleHash.Values;
+
+
+                    //foreach (var item in DegerListesi)
+                    //{
+                    //    foreach (var elem in item)
+                    //    {
+                    //        Console.Write(elem);
+                    //    }
+                    //    Console.WriteLine("");
+                    //}
+
+                    //for (int o = 0; o < hash.Length; o++)
+                    //{
+                    //    Console.Write(hash[o]);
+                    //}
+                    //Console.WriteLine(" - " + random[a] + "  ");
+
+                    foobar.BackgroundImageLayout = ImageLayout.Stretch;
+                    a++;
+                }
+            }
+        }
+
+        public string FilePath()
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    return dlg.FileName;
+                }
+
+                else
+                {
+                    ///todo yaparsın sen aslan
+                    return null;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            path = FilePath();
+            // Create a new Bitmap object from the picture file on disk,
+            // and assign that to the PictureBox.Image property
+            
+
+        }
+
+        private void btn_Mix_Click(object sender, EventArgs e)
+        {
+
+            var bitmap = new Bitmap(path);
+
+
+            Image img = bitmap; // a.png has 312X312 width and height
+
+            int width = img.Width / 4;
+            int height = img.Height / 4;
+            Bitmap[,] bmps = new Bitmap[4, 4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    bmps[i, j] = new Bitmap(width, height);
+                    Graphics g = Graphics.FromImage(bmps[i, j]);
+                    g.DrawImage(img, new Rectangle(0, 0, width, height), new Rectangle(j * width, i * height, width, height), GraphicsUnit.Pixel);
+                    g.Dispose();
+                    //Console.WriteLine("i =  " + i + "  j =  " + j + "  " + bmps[i, j].Width + "  -  " + bmps[i, j].Height);
+                }
+            }
+
+            int a = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    Button foobar = (Button)Controls.Find("btn_P" + a, false)[0];
+                    foobar.BackgroundImage = bmps[i, k];
+                    foobar.BackgroundImageLayout = ImageLayout.Stretch;
+                    a++;
+                }
+            }
+
+            Mix(bmps);
+
+        }
+
+        private void btnClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if(buttonId == -1){
+                buttonId = Int32.Parse(button.Name.Substring(5, button.Name.Length - 5));
+                //Controls.Find("btn_P"+)
+            }
+            else if(buttonId != Int32.Parse(button.Name.Substring(5, button.Name.Length - 5))){
+
+                int secondId = Int32.Parse(button.Name.Substring(5, button.Name.Length - 5));
+
+                Button first = (Button) Controls.Find("btn_P" + buttonId, false)[0];
+                Button second = (Button)Controls.Find("btn_P" + secondId, false)[0];
+
+                Image tmp = first.BackgroundImage;
+
+                first.BackgroundImage = second.BackgroundImage;
+                second.BackgroundImage = tmp;
+
+                byte[] firstHash;
+                byte[] secondHash;
+
+                shuffleHash.TryGetValue(buttonId, out firstHash);
+                shuffleHash.TryGetValue(secondId, out secondHash);
+
+                shuffleHash[buttonId] = secondHash;
+                shuffleHash[secondId] = firstHash;
+
+                buttonId = -1;
+
+            }
+
+        }
+    }
+
+}
